fae579dd66779de44da2ec25cafdcad6c8ef2c0c
KOUAM KAMDEM HERVE <hkouamherve145@gmail.com>
2024-10-22 17:25:02 -0400

    initialisation des paiemement
    ........................

---
+++ AppPaiements/Utilisateur.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Utilisateur
+    {
+        public string nom {  get; set; }
+        public List<Paiement> Paiements { get; set; }
+
+        public Utilisateur(string nom)
+        
+        {
+             string Nom = nom;
+             Paiements = new List<Paiement>();
+        
+        }
+        public void AfficherInfo()
+        {
+            Console.WriteLine($"nom....{nom}... ");
+            Console.WriteLine($"Liste de vos paiement :");
+            foreach (Paiement p in Paiements)
+            {
+                Console.Write("---");
+                    p.AfficherDetails();
+            }
+        }
+    }
+}
