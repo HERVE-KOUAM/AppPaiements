195728f35225427e233ec12ff333502d8d861e0b
KOUAM KAMDEM HERVE <hkouamherve145@gmail.com>
2024-10-22 14:19:17 -0400

    creation des carte et affichafe
    ici jai fini de creer les carte et je les push
    fin de letape 4 du tp

---
+++ AppPaiements/CarteCredit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class CarteCredit : Paiement
+    {
+        public int NumeroCarte { get; set; }
+
+        public CarteCredit(int numeroCarte , string description ,double montant) : base( montant, description)
+        {
+            NumeroCarte = numeroCarte;
+        }
+
+       
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($" numero carte  : {NumeroCarte} Descrition : { Description} , Montant {Montant} ");
+
+            
+        }
+    }
+}
---
+++ AppPaiements/Program.cs
@@ -0,0 +1,23 @@
+﻿using AppPaiements;
+using System.Reflection.Metadata;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // creation d'instances de carte 
+
+        CarteCredit carte1 = new CarteCredit(numeroCarte: 001, description : "Debit",  montant : 12500);
+        CarteCredit carte2 = new CarteCredit(numeroCarte: 002, description : "credit", montant: -10500);
+        CarteCredit carte3 = new CarteCredit(numeroCarte: 003, description: "credit", montant: -20000);
+
+
+        // affichage a lecran 
+
+        carte1.AfficherDetails();
+        carte2.AfficherDetails();
+        carte3.AfficherDetails();
+
+
+    }
+}
